b94ca19353467d191d824aff6f8c8502a28cd974
AndreaDotheCruz <56932254+AndreaDotheCruz@users.noreply.github.com>
2021-03-08 14:50:53 -0600

    practica3ver1

--- app/Http/Controllers/ListaController.php
+++ app/Http/Controllers/ListaController.php
@@ -14,23 +14,7 @@ class ListaController extends Controller
         $estudiante= estudiantes::all();
         //return $estudiante;
         $texto=trim($request->get('texto'));
-        $estudiante=DB::table('estudiantes')->select('Matricula', 'Nombre', 'Direccion')->where('Matricula', '=', $texto)->paginate(10);
+        $estudiante=DB::table('estudiantes')->select('Matricula', 'Nombre', 'Direccion')->where('Nombre','like',"%$texto%")->paginate(10);
         return view ('VistaEstudiantes', compact('estudiante'));
     }
 }
-/*
-
-$str = explode(' ',$_POST['str']);
-$resultado = DB::table('empleados')
-            ->select('nombres','apellidos','cargo')
-            ->where('estado','a')
-            ->where(function($query) use($str) {
-                foreach($str as $s) {
-                    $query = $query->orWhere('nombres','like',"%$s%");
-                    $query = $query->orWhere('apellidos','like',"%$s%");
-              }
-              
-            })
-            ->take(5)
-            ->get();*/
-            
